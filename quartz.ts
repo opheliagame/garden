f088a69e801746005fb5db7d2f6640674e63e1b7
opheliagame <21999998+opheliagame@users.noreply.github.com>
2026-07-22 20:04:50 +0900

    added condition is-index to show recent notes on index page

--- quartz.ts
+++ quartz.ts
@@ -1,5 +1,7 @@
-import { loadQuartzConfig, loadQuartzLayout } from "./quartz/plugins/loader/config-loader"
+import { loadQuartzConfig } from "./quartz/plugins/loader/config-loader"
+import { registerCondition } from "./quartz/plugins/loader/conditions"
+
+registerCondition("is-index", (props) => props.fileData.slug === "index")
 
 const config = await loadQuartzConfig()
 export default config
-export const layout = await loadQuartzLayout()
